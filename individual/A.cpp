9e9116b1fc3f2f7006167020a85fd50be41b41cc
TesseractExW <w.nutdanaith12@outlook.com>
2026-08-10 00:05:59 +0700

    Update A.cpp

--- individual/A.cpp
+++ individual/A.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <cstdint>
 
 // math, unsigned int 
 int main(int argc, char *argv[]) {
     std::cin.tie(nullptr)->sync_with_stdio(false);    
 
-    std::size_t first, best = 0;
+    std::uint64_t first, best = 0;
     std::cin >> first;
     best = first;
 
-    std::size_t crr;
+    std::uint64_t crr;
     while (std::cin >> crr && crr > 0) {
         best = std::max(best, crr);
     }
