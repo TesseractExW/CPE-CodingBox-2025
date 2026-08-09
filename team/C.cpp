765066263e65acf5334e2cab48f9448d03ab65fa
TesseractExW <w.nutdanaith12@outlook.com>
2026-08-10 00:17:05 +0700

    Update C.cpp

--- team/C.cpp
+++ team/C.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include <cstdint>
 
 // math, geometry
 int main(int argc, char *argv[]) {
     std::cin.tie(nullptr)->sync_with_stdio(false);
 
-    std::size_t n;
+    std::uint64_t n;
     std::cin >> n;
 
     std::cout << n * n * n << ' ' ;
