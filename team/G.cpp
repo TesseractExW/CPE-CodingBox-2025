036d626aa780f8e25bb2453dd25648b782f0397b
TesseractExW <w.nutdanaith12@outlook.com>
2026-08-10 00:23:58 +0700

    Update G.cpp

--- team/G.cpp
+++ team/G.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#include <cstdint>
 
 // fast ceiling
 int main(int argc, char *argv[]) {
     std::cin.tie(nullptr)->sync_with_stdio(false);
 
-    std::size_t l, k, m, n;
+    std::uint64_t l, k, m, n;
     std::cin >> l >> k;
     std::cin >> m >> n;
 
-    std::size_t total = 0;
-    for (std::size_t i = 0; i < m * n; i++) {
-        std::size_t height;
+    std::uint64_t total = 0;
+    for (std::uint64_t i = 0; i < m * n; i++) {
+        std::uint64_t height;
         std::cin >> height;
 
-        total += std::max(0uz, height -  k);
+        total += std::max(0ULL, height -  k);
     }
 
     std::cout << (total + l - 1) / l;
