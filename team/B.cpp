1d3c0099fc815a969248dc51b3fab8d3a57b6634
TesseractExW <w.nutdanaith12@outlook.com>
2026-08-10 00:16:39 +0700

    Update B.cpp

--- team/B.cpp
+++ team/B.cpp
@@ -2,19 +2,20 @@
 #include <vector>
 #include <map>
 #include <bit>
+#include <cstdint>
 
 // binary search, iterator, prefix sum
 int main(int argc, char *argv[]) {
     std::cin.tie(nullptr)->sync_with_stdio(false);
     
-    std::size_t n; 
+    std::uint64_t n; 
     std::cin >> n;
     n = 63 - std::countl_zero(n);
     
-    std::vector<std::size_t> prv = { 0 };
+    std::vector<std::uint64_t> prv = { 0 };
     
-    for (std::size_t i = 0; i < n; i++) {
-        std::vector<std::size_t> crr(prv.size() * 2);
+    for (std::uint64_t i = 0; i < n; i++) {
+        std::vector<std::uint64_t> crr(prv.size() * 2);
 
         for (std::size_t j = 0; j < crr.size(); j++) {
             std::cin >> crr[j];
@@ -24,17 +25,18 @@ int main(int argc, char *argv[]) {
         std::swap(prv, crr);
     }
 
-    std::map<std::size_t, std::size_t> _map;
+    std::map<std::uint64_t, std::uint64_t> _map;
     
     for (std::size_t i = 0; i < prv.size(); i++) {
+        std::uint64_t ind = (1ULL << n) + (std::uint64_t)i;
         if (_map.count(prv[i])) {
-            _map[prv[i]] = std::min(_map[prv[i]], (1 << n) + i);
+            _map[prv[i]] = std::min(_map[prv[i]], ind);
         } else {
-            _map[prv[i]] = (1 << n) + i;
+            _map[prv[i]] = ind;
         }
     }
     
-    std::size_t prefix = std::numeric_limits<std::size_t>::max();
+    std::uint64_t prefix = std::numeric_limits<std::uint64_t>::max();
     for (auto &[val, i] : _map) {
         prefix = std::min(prefix, i);
         i = prefix;
@@ -44,10 +46,10 @@ int main(int argc, char *argv[]) {
     std::cin >> q;
     
     for (int i = 0; i < q; i++) {
-        std::size_t x;
+        std::uint64_t x;
         std::cin >> x;
 
-        std::map<std::size_t, std::size_t>::iterator it = _map.upper_bound(x);
+        std::map<std::uint64_t, std::uint64_t>::iterator it = _map.upper_bound(x);
         if (it == _map.begin()) {
             std::cout << -1 << '\n';
         } else {
